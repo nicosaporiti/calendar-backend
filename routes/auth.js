3b8ed754a9cff9f2e7172b132196500eb6793179
Nicolas Saporiti <nicolas.saporiti@gmail.com>
2021-11-13 21:23:57 -0300

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+/* Rutas de usuarios / Auth
+    host + api/auth
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const router = Router();
+
+const {
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+} = require('../controllers/auth');
+
+router.post(
+  '/new',
+  [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe tener al menos 6 caracteres').isLength({
+      min: 6,
+    }),
+  ],
+  crearUsuario
+);
+
+router.post(
+  '/',
+  [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe tener al menos 6 caracteres').isLength({
+      min: 6,
+    }),
+  ],
+  loginUsuario
+);
+
+router.get('/renew', revalidarToken);
+
+module.exports = router;
